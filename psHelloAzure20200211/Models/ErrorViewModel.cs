91a87a94dc3fc208e5b09505bb6b2eb31e2a07a3
Micheal Murphy <micheal_murphy@countrywide.com>
2020-02-11 11:37:45 -0800

    Add project files.

---
+++ psHelloAzure20200211/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace psHelloAzure20200211.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
